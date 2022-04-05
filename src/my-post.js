fd02d2526832f6b329622dc439a83f1bd5cad1f2
hajihyeon <hajh0729@naver.com>
2022-04-05 09:04:21 +0900

    use-hook

---
+++ src/my-post.js
@@ -0,0 +1,26 @@
+import "react-notion/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import { NotionRenderer } from "react-notion";
+import React, { useState, useEffect } from 'react';
+
+function MyPost(){
+    const [response, setResponse] = useState({});
+
+    useEffect(() => {
+        console.log('use effect 실행');
+        const NOTION_PAGE_ID = 'Resume-144e51cee8814ba09e6ce84bc04c29a0';
+        fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
+          .then(res => res.json())
+          .then((resJson) => {
+            setResponse(resJson);
+          });
+      }, [])
+
+      return(
+        <div style={{ backgroundColor: 'white', textAlign: 'left' }}>
+            <NotionRenderer blockMap={response}  fullPage={true}  />
+        </div>
+      )
+}
+
+export default MyPost;
