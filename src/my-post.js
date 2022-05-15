456ddffa4158ec8a1630c342d61bc843ed0c21cf
hajihyeon <hajh0729@naver.com>
2022-05-15 10:25:59 +0900

    kor resume

--- src/my-post.js
+++ src/my-post.js
@@ -8,7 +8,7 @@ function MyPost(){
 
     useEffect(() => {
         console.log('use effect 실행');
-        const NOTION_PAGE_ID = 'Resume-144e51cee8814ba09e6ce84bc04c29a0';
+        const NOTION_PAGE_ID = 'Resume_KOR-144e51cee8814ba09e6ce84bc04c29a0';
         fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
           .then(res => res.json())
           .then((resJson) => {
